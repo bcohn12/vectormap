9e8637b03c0aaae3ba39ce915d3b762a27dc8a38
bcohn12 <boss@Cheiron.local>
2014-10-27 15:32:24 -0700

    added progress bar

--- activation_bound_matrices.m
+++ activation_bound_matrices.m
@@ -11,8 +11,10 @@ randpoint_num = length(unitvectors(:,1));
 lowerbound_mat = [];
 upperbound_mat = [];
 lenmat = length(unitvectors(:,1));
+
+h = waitbar(0, 'wait');
 % iterate over every vector in unitvectors
-parfor row= 1:lenmat
+for row= 1:lenmat
     forcevec = [unitvectors(row,1), unitvectors(row,2), unitvectors(row,3), 0, 0, 0]; % current
     Fend = forcevec'; %defining an output direction (vector)
     Fend = Fend/norm(Fend,2);
@@ -24,9 +26,8 @@ parfor row= 1:lenmat
     %Paste in the upper bound vector of activations
     hi_entry = [forcevec, hivec];
     upperbound_mat = [upperbound_mat ; hi_entry];
-    if mod(row, 1000) == 0
-        fprintf('Vectorpoint: : %u/ %u  \n',row, lenmat);
-    end
+    waitbar(row/lenmat)
 end
+close(h)
 
 end
